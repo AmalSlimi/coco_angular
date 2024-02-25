7c82864ec00f1cc375b2211d37cc5f8f30f3982c
SlimiAmal <slimi.amal@esprit.tn>
2024-02-25 02:06:17 +0100

    integrated backoffice and frontoffice template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeFrontComponent } from './frontOffice/home-front/home-front.component';
+import { HomeBackComponent } from './backOffice/home-back/home-back.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'user', component:HomeFrontComponent},
+{path: 'admin', component:HomeBackComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterBackComponent } from './backOffice/footer-back/footer-back.component';
+import { HeaderBackComponent } from './backOffice/header-back/header-back.component';
+import { HomeBackComponent } from './backOffice/home-back/home-back.component';
+import { HomeFrontComponent } from './frontOffice/home-front/home-front.component';
+import { FooterFrontComponent } from './frontOffice/footer-front/footer-front.component';
+import { HeaderFrontComponent } from './frontOffice/header-front/header-front.component';
+import { SidebarBackComponent } from './backOffice/sidebar-back/sidebar-back.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterBackComponent,
+    HeaderBackComponent,
+    HomeBackComponent,
+    HomeFrontComponent,
+    FooterFrontComponent,
+    HeaderFrontComponent,
+    SidebarBackComponent
+    
   ],
   imports: [
     BrowserModule,
