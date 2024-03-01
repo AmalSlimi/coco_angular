f539e42690dadc91c07db7a96aa9e8fabe593949
SlimiAmal <slimi.amal@esprit.tn>
2024-03-02 00:50:08 +0100

    backOffice / frontOffice template integration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,87 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {RouterModule} from '@angular/router';
+import {NgModule} from '@angular/core';
+import {DashboardDemoComponent} from './backOffice/demo/view/dashboarddemo.component';
+import {FormLayoutDemoComponent} from './backOffice/demo/view/formlayoutdemo.component';
+import {FloatLabelDemoComponent} from './backOffice/demo/view/floatlabeldemo.component';
+import {InvalidStateDemoComponent} from './backOffice/demo/view/invalidstatedemo.component';
+import {PanelsDemoComponent} from './backOffice/demo/view/panelsdemo.component';
+import {OverlaysDemoComponent} from './backOffice/demo/view/overlaysdemo.component';
+import {MediaDemoComponent} from './backOffice/demo/view/mediademo.component';
+import {MessagesDemoComponent} from './backOffice/demo/view/messagesdemo.component';
+import {MiscDemoComponent} from './backOffice/demo/view/miscdemo.component';
+import {EmptyDemoComponent} from './backOffice/demo/view/emptydemo.component';
+import {ChartsDemoComponent} from './backOffice/demo/view/chartsdemo.component';
+import {FileDemoComponent} from './backOffice/demo/view/filedemo.component';
+import {DocumentationComponent} from './backOffice/demo/view/documentation.component';
+import {AppMainComponent} from './backOffice/app.main.component';
+import {AppNotfoundComponent} from './backOffice/pages/app.notfound.component';
+import {AppErrorComponent} from './backOffice/pages/app.error.component';
+import {AppAccessdeniedComponent} from './backOffice/pages/app.accessdenied.component';
+import {AppLoginComponent} from './backOffice/pages/app.login.component';
+import {InputDemoComponent} from './backOffice/demo/view/inputdemo.component';
+import {ButtonDemoComponent} from './backOffice/demo/view/buttondemo.component';
+import {TableDemoComponent} from './backOffice/demo/view/tabledemo.component';
+import {ListDemoComponent} from './backOffice/demo/view/listdemo.component';
+import {TreeDemoComponent} from './backOffice/demo/view/treedemo.component';
+import {IconsComponent} from './backOffice/utilities/icons.component';
+import {AppCrudComponent} from './backOffice/pages/app.crud.component';
+import {AppCalendarComponent} from './backOffice/pages/app.calendar.component';
+import {AppTimelineDemoComponent} from './backOffice/pages/app.timelinedemo.component';
+import {AppInvoiceComponent} from './backOffice/pages/app.invoice.component';
+import {AppHelpComponent} from './backOffice/pages/app.help.component';
+import {BlocksComponent} from './backOffice/blocks/blocks/blocks.component';
 import { HomeFrontComponent } from './frontOffice/home-front/home-front.component';
-import { HomeBackComponent } from './backOffice/home-back/home-back.component';
-
-const routes: Routes = [
-{path: 'user', component:HomeFrontComponent},
-{path: 'admin', component:HomeBackComponent},
-];
+import { HeaderFrontComponent } from './frontOffice/header-front/header-front.component';
+import { CarousalComponent } from './frontOffice/carousal/carousal.component';
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot([
+            {
+                path: '', component: AppMainComponent,
+                children: [
+                    {path: '', component: DashboardDemoComponent},
+                    {path: 'uikit/formlayout', component: FormLayoutDemoComponent},
+                    {path: 'uikit/floatlabel', component: FloatLabelDemoComponent},
+                    {path: 'uikit/invalidstate', component: InvalidStateDemoComponent},
+                    {path: 'uikit/input', component: InputDemoComponent},
+                    {path: 'uikit/button', component: ButtonDemoComponent},
+                    {path: 'uikit/table', component: TableDemoComponent},
+                    {path: 'uikit/list', component: ListDemoComponent},
+                    {path: 'uikit/tree', component: TreeDemoComponent},
+                    {path: 'uikit/panel', component: PanelsDemoComponent},
+                    {path: 'uikit/overlay', component: OverlaysDemoComponent},
+                    {path: 'uikit/menu', loadChildren: () => import('./backOffice/demo/view/menus/menus.module').then(m => m.MenusModule)},
+                    {path: 'uikit/media', component: MediaDemoComponent},
+                    {path: 'uikit/message', component: MessagesDemoComponent},
+                    {path: 'uikit/misc', component: MiscDemoComponent},
+                    {path: 'uikit/charts', component: ChartsDemoComponent},
+                    {path: 'uikit/file', component: FileDemoComponent},
+                    {path: 'utilities/icons', component: IconsComponent},
+                    {path: 'pages/crud', component: AppCrudComponent},
+                    {path: 'pages/calendar', component: AppCalendarComponent},
+                    {path: 'pages/timeline', component: AppTimelineDemoComponent},
+                    {path: 'pages/invoice', component: AppInvoiceComponent},
+                    {path: 'pages/help', component: AppHelpComponent},
+                    {path: 'pages/empty', component: EmptyDemoComponent},
+                    {path: 'documentation', component: DocumentationComponent},
+                    {path: 'blocks', component: BlocksComponent},
+                ]
+                
+            },
+            {path: 'error', component: AppErrorComponent},
+            {path: 'access', component: AppAccessdeniedComponent},
+            {path: 'notfound', component: AppNotfoundComponent},
+            {path: 'login', component: AppLoginComponent},
+
+            {path: 'coco',component:HomeFrontComponent},
+            {path: 'coco1',component:HeaderFrontComponent},
+            {path: 'coco2',component:CarousalComponent},
+
+            {path: '**', redirectTo: '/notfound'},
+        ], {scrollPositionRestoration: 'enabled'})
+    ],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/assets/frontOffice/js/main.js
+++ src/assets/frontOffice/js/main.js
@@ -1,414 +1,252 @@
-(function($) {
-    "use strict";
-    /* -------------------------------------
-               Prealoder
-         -------------------------------------- */
-    $(window).on('load', function(event) {
-        $('.js-preloader').delay(500).fadeOut(500);
-    });
+/*  ---------------------------------------------------
+    Template Name: Aler
+    Description:  Aler property HTML Template
+    Author: Colorlib
+    Author URI: https://colorlib.com
+    Version: 1.0
+    Created: Colorlib
+---------------------------------------------------------  */
 
-    /* -------------------------------------
-            Buy Now Button
-    -------------------------------------- */
-    $('body').append("<a href='https://themeforest.net/checkout/84095926/create_account?_ga=2.258938703.497479303.1645493536-1768322795.1645493536' class='buy-now-btn' target='_blank'><img src='assets/img/envato.png' alt='envato'/>Buy Now</a>"); 
+'use strict';
 
-    /* -------------------------------------
-          Open Search
-    -------------------------------------- */
-    $('.searchbtn').on('click', function() {
-        $('.search-area').toggleClass('open');
-    });
+(function ($) {
 
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
 
-    /* -------------------------------------
-          Language Dropdown
-    -------------------------------------- */
-    $(".language-option").each(function() {
-        var each = $(this)
-        each.find(".lang-name").html(each.find(".language-dropdown-menu a:nth-child(1)").text());
-        var allOptions = $(".language-dropdown-menu").children('a');
-        each.find(".language-dropdown-menu").on("click", "a", function() {
-            allOptions.removeClass('selected');
-            $(this).addClass('selected');
-            $(this).closest(".language-option").find(".lang-name").html($(this).text());
+        /*------------------
+            Property filter
+        --------------------*/
+        $('.property-controls li').on('click', function () {
+            $('.property-controls li').removeClass('active');
+            $(this).addClass('active');
         });
-    })
-    $('.user-option').on('click', function() {
-        $('.user-menuitem').toggleClass('open');
+        if ($('.property-filter').length > 0) {
+            var containerEl = document.querySelector('.property-filter');
+            var mixer = mixitup(containerEl);
+        }
     });
 
-    /* -------------------------------------
-           Progressbar 
-    // -------------------------------------- */
-
-    $(window).scroll(function() {
-        // if ($(window).scrollTop() > 100) { // scroll down abit and get the action   
-
-        $('.progress-bar').each(function() {
-            $(this).find('.progress-content').animate({
-                width: $(this).attr('data-percentage')
-            }, 2000);
-
-            $(this).find('.progress-number-mark').animate({ left: $(this).attr('data-percentage') }, {
-                duration: 2000,
-                step: function(now, fx) {
-                    var data = Math.round(now);
-                    $(this).find('.percent').html(data + '%');
-                }
-            });
-        });
-        // }
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
     });
 
+    //Canvas Menu
+    $(".canvas-open").on('click', function () {
+        $(".offcanvas-menu-wrapper").addClass("show-offcanvas-menu-wrapper");
+        $(".offcanvas-menu-overlay").addClass("active");
+    });
 
-    /* -------------------------------------
-           Hero Slider 
-    // -------------------------------------- */
-    var hero_slider = new Swiper(".hero-slider-one", {
-        spaceBetween: 0,
-        slidesPerView: 1,
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: true,
-        },
-        fadeEffect: { crossFade: true },
-        autoplayDisableOnInteraction: true,
-        effect: "fade",
+    $(".canvas-close, .offcanvas-menu-overlay").on('click', function () {
+        $(".offcanvas-menu-wrapper").removeClass("show-offcanvas-menu-wrapper");
+        $(".offcanvas-menu-overlay").removeClass("active");
+    });
+
+    /*------------------
+		Navigation
+	--------------------*/
+    $(".nav-menu").slicknav({
+        prependTo: '#mobile-menu-wrap',
+        allowParentLinks: true
+    });
+
+    /*------------------
+        Carousel Slider
+    --------------------*/
+    var hero_s = $(".hs-slider");
+    hero_s.owlCarousel({
         loop: true,
-        speed: 1500,
-        observer: true,
-        observeParents: true,
-        navigation: {
-            nextEl: ".hero-slide-next",
-            prevEl: ".hero-slide-prev"
-        },
-
+        margin: 20,
+        nav: true,
+        items: 1,
+        dots: false,
+        navText: ['<i class="arrow_left"></i>', '<i class="arrow_right"></i>'],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true
     });
-    /* -------------------------------------
-           Service Slider 
-     -------------------------------------- */
-    var service_slider_one = new Swiper(".service-slider-one", {
+
+    /*------------------
+        Team Slider
+    --------------------*/
+    $(".fp-slider").owlCarousel({
         loop: true,
-        spaceBetween: 30,
-        autoplay: {
-            delay: 2000,
-            disableOnInteraction: true,
-        },
-        slidesPerView: 3,
-        speed: 1500,
-        freeMode: true,
-        observer: true,
-        observeParents: true,
-        watchSlidesVisibility: true,
-        watchSlidesProgress: true,
-        navigation: {
-            nextEl: ".service-next",
-            prevEl: ".service-prev",
-        },
-        breakpoints: {
+        margin: 0,
+        items: 1,
+        dots: false,
+        nav: true,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        navText: ['<i class="arrow_left"></i>', '<i class="arrow_right"></i>'],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true
+    });
+
+    /*------------------
+        Testimonial Slider
+    --------------------*/
+    $(".testimonial-slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 2,
+        dots: false,
+        nav: true,
+        navText: ['<i class="arrow_left"></i>', '<i class="arrow_right"></i>'],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        responsive: {
             0: {
-                slidesPerView: 1
-
+                items: 1
             },
             768: {
-                slidesPerView: 1.5,
-                centeredSlides: true
+                items: 2
+            }
+        }
+    });
 
+    /*------------------
+        Logo Slider
+    --------------------*/
+    $(".lc-slider").owlCarousel({
+        loop: true,
+        margin: 115,
+        items: 6,
+        dots: false,
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            480: {
+                items: 3
+            },
+            768: {
+                items: 4
             },
             992: {
-                slidesPerView: 2
-
+                items: 5
             },
             1200: {
-                slidesPerView: 3
-
-
+                items: 6
             }
         }
     });
 
-    /* -------------------------------------
-           Partner Slider 
-     -------------------------------------- */
-    var service_slider = new Swiper(".partner-slider", {
+    /*------------------------
+        Property pic slider
+    -------------------------*/
+    $(".property-pic-slider").owlCarousel({
         loop: true,
-        spaceBetween: 30,
-        autoplay: {
-            delay: 2000,
-            disableOnInteraction: true,
-        },
-        speed: 1500,
-        breakpoints: {
-            0: {
-                slidesPerView: 2
-
-            },
-            576: {
-                slidesPerView: 3
-
-            },
-            768: {
-                slidesPerView: 4
-
-            },
-            1200: {
-                slidesPerView: 6
-
-
-            }
-        }
+        margin: 0,
+        items: 1,
+        dots: false,
+        nav: true,
+        navText: ['<i class="arrow_left"></i>', '<i class="arrow_right"></i>'],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true
     });
-    /* ----------------------------------------
-           Magnific Popup Video
-     ------------------------------------------*/
-    $('.video-play').magnificPopup({
-        type: 'iframe',
-        mainClass: 'mfp-fade',
-        preloader: true,
-    });
-    /* -------------------------------------
-           Testimonials Slider 
-     -------------------------------------- */
-    var testimonial_slider_one = new Swiper(".testimonial-slider-one", {
+
+    /*------------------------
+        Sidebar Feature slider
+    -------------------------*/
+    $(".sf-slider").owlCarousel({
         loop: true,
-        spaceBetween: 0,
-        autoplay: {
-            delay: 2000,
-            disableOnInteraction: true,
-        },
-        slidesPerView: 3,
-        speed: 1500,
-        centeredSlides: true,
-        breakpoints: {
-            0: {
-                slidesPerView: 1
-
-            },
-            768: {
-                slidesPerView: 2
-
-            },
-            1200: {
-                slidesPerView: 3
-
-
-            }
-        }
+        margin: 0,
+        items: 1,
+        dots: true,
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true
     });
-    var testimonial_slider_two = new Swiper(".testimonial-slider-two", {
+
+    /*------------------------
+        Agent slider
+    -------------------------*/
+    $(".as-slider").owlCarousel({
         loop: true,
-        spaceBetween: 0,
-        // autoplay: {
-        //     delay: 2000,
-        //     disableOnInteraction: true,
-        // },
-        speed: 1500,
-        centeredSlides: true,
-        pagination: {
-            el: ".testimonial-pagination",
-            clickable: true,
-        },
-        breakpoints: {
-            0: {
-                slidesPerView: 1
+        margin: 0,
+        items: 1,
+        dots: false,
+        nav: true,
+        navText: ['<i class="arrow_left"></i>', '<i class="arrow_right"></i>'],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true
+    });
 
-            },
-            768: {
-                slidesPerView: 1.5
+    /*------------------
+        Video Popup
+    --------------------*/
+    $('.video-popup').magnificPopup({
+        type: 'iframe'
+    });
 
-            },
-            992: {
-                slidesPerView: 2
+    /*------------------
+        Nice Select
+    --------------------*/
+    $('select').niceSelect();
 
-            },
-            1200: {
-                slidesPerView: 3
+    /*-------------------
+		Radio Btn
+	--------------------- */
+    $(".cb-item label").on('click', function () {
+        $(".cb-item label").removeClass('active');
+        $(this).addClass('active');
+    });
 
+    /*-------------------
+		Range Wrap
+	--------------------- */
 
-            }
+    //room size
+    $("#roomsize-range").slider({
+        range: true,
+        min: 0,
+        max: 2100,
+        step: 300,
+        values: [300, 1200],
+        slide: function (event, ui) {
+            $("#roomsizeRange").val("[" + ui.values[0] + "-" + ui.values[1] + "]" + "sqft");
         }
     });
-    /* -------------------------------------
-           Blog Slider 
-     -------------------------------------- */
-    var blog_slider_one = new Swiper(".blog-slider-one", {
-        loop: true,
-        spaceBetween: 30,
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: true,
-        },
-        slidesPerView: 3,
-        speed: 1500,
-        navigation: {
-            nextEl: ".blog-next",
-            prevEl: ".blog-prev",
-        },
-        breakpoints: {
-            0: {
-                slidesPerView: 1
+    $("#roomsizeRange").val("[" + $("#roomsize-range").slider("values", 0) + "-" + $("#roomsize-range").slider("values", 1) + "]" + "sqft");
 
-            },
-            768: {
-                slidesPerView: 1.5,
-                centeredSlides: true
-
-            },
-            992: {
-                slidesPerView: 2
-
-            },
-            1200: {
-                slidesPerView: 3
-
-            }
+    //price range
+    $("#price-range").slider({
+        range: true,
+        min: 0,
+        max: 1500,
+        step: 100,
+        values: [100, 1000],
+        slide: function (event, ui) {
+            $("#priceRange").val("[ " + ui.values[0] + " - " + ui.values[1] + " ]" + " $");
         }
     });
-    /* -------------------------------------
-          Single Portfolio Slider 
-    -------------------------------------- */
-    var single_portfolio_slider = new Swiper(".single-portfolio-slider", {
-        loop: true,
-        spaceBetween: 30,
-        autoplay: {
-            delay: 2000,
-            disableOnInteraction: true,
-        },
-        slidesPerView: 1,
-        speed: 1500,
-        pagination: {
-            el: ".single-portfolio-pagination",
-            clickable: true,
-        },
+    $("#priceRange").val("[ " + $("#price-range").slider("values", 0) + " - " + $("#price-range").slider("values", 1) + " ]" + " $");
 
+     //Text editor
+    $('.texteditor-content').richText();
 
-    });
-
-
-    /* -------------------------------------
-          Mobile Topbar 
-    -------------------------------------- */
-
-    $('.mobile-top-bar').on('click', function() {
-        $('.header-top-right').addClass('open')
-    });
-    $('.close-header-top').on('click', function() {
-        $('.header-top-right').removeClass('open')
-    });
-
-    /* -------------------------------------
-          sticky Header
-    -------------------------------------- */
-    var wind = $(window);
-    var sticky = $('.header-wrap');
-    wind.on('scroll', function() {
-        var scroll = wind.scrollTop();
-        if (scroll < 100) {
-            sticky.removeClass('sticky');
-        } else {
-            sticky.addClass('sticky');
+    $('.texteditor-switch').on('click', function () {
+        if(!$(this).hasClass('active')) {
+            $(".richText-btn[data-command='toggleCode']").click();
         }
     });
 
-    /*---------------------------------
-        Responsive mmenu
-    ---------------------------------*/
-    $('.mobile-menu a').on('click', function() {
-        $('.main-menu-wrap').addClass('open');
-        $('.mobile-bar-wrap.style2 .mobile-menu').addClass('open');
-    });
-
-    $('.mobile_menu a').on('click', function() {
-        $(this).parent().toggleClass('open');
-        $('.main-menu-wrap').toggleClass('open');
-    });
-
-    $('.menu-close').on('click', function() {
-        $('.main-menu-wrap').removeClass('open')
-    });
-    $('.mobile-top-bar').on('click', function() {
-        $('.header-top').addClass('open')
-    });
-    $('.close-header-top button').on('click', function() {
-        $('.header-top').removeClass('open')
-    });
-    var $offcanvasNav = $('.main-menu'),
-        $offcanvasNavSubMenu = $offcanvasNav.find('.sub-menu');
-    $offcanvasNavSubMenu.parent().prepend('<span class="menu-expand"><i class="las la-angle-down"></i></span>');
-
-    $offcanvasNavSubMenu.slideUp();
-
-    $offcanvasNav.on('click', 'li a, li .menu-expand', function(e) {
-        var $this = $(this);
-        if (($this.attr('href') === '#' || $this.hasClass('menu-expand'))) {
-            e.preventDefault();
-            if ($this.siblings('ul:visible').length) {
-                $this.siblings('ul').slideUp('slow');
-            } else {
-                $this.closest('li').siblings('li').find('ul:visible').slideUp('slow');
-                $this.siblings('ul').slideDown('slow');
-            }
-        }
-        if ($this.is('a') || $this.is('span') || $this.attr('class').match(/\b(menu-expand)\b/)) {
-            $this.parent().toggleClass('menu-open');
-        } else if ($this.is('li') && $this.attr('class').match(/\b('has-children')\b/)) {
-            $this.toggleClass('menu-open');
-        }
-    });
-
-    /*---------------------------------
-         Scroll animation
-    ----------------------------------*/
-      AOS.init();
-    /*-----------------------------------
-         Scroll to top
-    ----------------------------------*/
-
-    // Show or hide the  button
-    $(window).on('scroll', function(event) {
-        if ($(this).scrollTop() > 600) {
-            $('.back-to-top').fadeIn(200)
-        } else {
-            $('.back-to-top').fadeOut(200)
-        }
-    });
-
-
-    //Animate the scroll to top
-    $('.back-to-top').on('click', function(event) {
-        event.preventDefault();
-
-        $('html, body').animate({
-            scrollTop: 0,
-        }, 1500);
-    });
+     //Drag Upload
+    $('.feature-image-content').imageUploader();
 
 })(jQuery);
-
-
-
-// function to set a given theme/color-scheme
-function setTheme(themeName) {
-    localStorage.setItem('theme', themeName);
-    document.documentElement.className = themeName;
-}
-
-// function to toggle between light and dark theme
-function toggleTheme() {
-    if (localStorage.getItem('theme') === 'theme-dark') {
-        setTheme('theme-light');
-    } else {
-        setTheme('theme-dark');
-    }
-}
-
-// Immediately invoked function to set the theme on initial load
-(function () {
-    if (localStorage.getItem('theme') === 'theme-dark') {
-        setTheme('theme-dark');
-        document.getElementById('slider').checked = false;
-    } else {
-        setTheme('theme-light');
-        document.getElementById('slider').checked = true;
-    }
-})();
